fa80bec9cd99bfc389bdc05292fe777ba1bd2723
Agnieszka <agnieszka.blicharska@gmail.com>
2021-09-27 20:51:33 +0200

    poprawiony widok główny

--- local_ads_table/settings.py
+++ local_ads_table/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'local_ads_table.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -76,11 +77,9 @@ WSGI_APPLICATION = 'local_ads_table.wsgi.application'
 
 DATABASES = {
     'default': {
-        'HOST': '127.0.0.1',
-        'NAME': 'local_ads',
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'USER': 'postgres',
-        'PASSWORD': 'coderslab',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'local_ads_db',
+
     }
 }
 
@@ -122,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- main_app/models.py
+++ main_app/models.py
@@ -3,7 +3,15 @@ from django.db import models
 # Create your models here.
 from django.db import models
 
-# tabela ogłoszeniodawców
+# możliwe kategorie ogłoszeń
+
+list_of_categories = ((0, 'inne'),
+                      (1, 'odzież'),
+                      (2, 'motoryzacja'),
+                      (3, 'usługi'),
+                      (4, 'żywność'),
+                      (5, 'sport'),)
+
 
 class User(models.Model):
     name = models.CharField(max_length=64)
@@ -11,19 +19,17 @@ class User(models.Model):
     phone_number = models.IntegerField(null=True)
     email = models.CharField(max_length=64, unique=True)
 
-# możliwe kategorie ogłoszeń
+    def __str__(self):
+        return self.surname
 
-list_of_categories = ((0, 'inne'),
-                      (1, 'odzież'),
-                      (2, 'motoryzacja'),
-                      (3, 'usługi'),
-                      (4, 'żywność'))
 
 # tabela ogłoszeń
-
 class Ad(models.Model):
     title = models.CharField(max_length=64, null=True)
     content = models.TextField(null=True)
     active = models.BooleanField(default=True)
     category = models.IntegerField(choices=list_of_categories, default=0)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
